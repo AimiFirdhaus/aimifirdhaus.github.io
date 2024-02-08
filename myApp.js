247784b8868b34d2971b156ead5d35b5bed30d13
Aimi Firdhaus Shafie <66451785+AimiFirdhaus@users.noreply.github.com>
2024-02-08 07:39:48 +0000

    huhu

--- myApp.js
+++ myApp.js
@@ -1,6 +1,18 @@
 let express = require("express");
 let app = express();
 
-app.get("/", (req, res) => res.send(console.log("Hello World")));
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
 module.exports = app;
